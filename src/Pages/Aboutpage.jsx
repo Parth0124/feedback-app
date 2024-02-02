1e458ea3e7b0575e618b63107e14e5e3f7cc55b9
Parth0124 <abhangparth@gmail.com>
2024-02-02 16:23:36 +0530

    update2

--- src/Pages/Aboutpage.jsx
+++ src/Pages/Aboutpage.jsx
@@ -1,8 +1,19 @@
+import Card from "../components/shared/card"
+
 function Aboutpage() {
   return (
-    <div>
-      About
+   <Card>
+    <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a react based feedback app to leave feedbacks and ratings for a particular product or service.</p>
+        <p>Version 1.0.0</p>
+
+
+        <p>
+            <a href="/">Back To Home</a>
+        </p>
     </div>
+   </Card>
   )
 }
 
